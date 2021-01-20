ed4aae4535130b4160802e05c151c96935112a35
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-01-20 15:17:42 -0500

    Gitacora saved 1 changed files

--- ts/main-scene.ts
+++ ts/main-scene.ts
@@ -138,8 +138,8 @@ class GameScene extends Phaser.Scene {
     let texto = this.object<Phaser.GameObjects.Text>('texto.prueba');
     let camera = { x: this.cameras.main.scrollX, y: this.cameras.main.scrollY };
     texto.setPosition(camera.x + 10, camera.y + 10);
-    texto.text = `Camera Position - x: ${Math.round(camera.x)} - y: ${Math.floor(camera.y)}\n`;
-    texto.text += `Pasto 1 Position - x: ${Math.round(fondo5a.x)} - y: ${Math.floor(fondo5a.y)}\n`;
+    texto.text = `Camera Position | x: ${Math.round(camera.x)} | y: ${Math.floor(camera.y)}\n`;
+    texto.text += `Pasto 1 Position | x: ${Math.round(fondo5a.x)} | y: ${Math.floor(fondo5a.y)}\n`;
     // texto.text += `Fondo.5.b (Ground b) Position - x: ${Math.round(fondo5b.x)} - y: ${Math.floor(fondo5b.y)}\n`;
 
     this.object<Phaser.GameObjects.Image>("fondo.1").setPosition(camera.x * 1 - 100, camera.y - 100);
