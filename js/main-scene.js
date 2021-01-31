b7cddddc15a6c607d4d7c146f268063e3c8b6d46
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-01-31 18:32:10 -0500

    add project

--- js/main-scene.js
+++ js/main-scene.js
@@ -85,18 +85,22 @@ var GameScene = /** @class */ (function (_super) {
         mainChar.anims.play('attack');
         var camera = this.cameras.main.startFollow(mainChar, false, 0.3, 0.3);
         // camera.setBounds(0, 0, this.scale.width * 2, 0)
-        this.object('texto.debug', this.add.text(10, 10, 'Camera Position').setOrigin(0, 0));
+        if (this.physics.config.debug) {
+            this.object('texto.debug', this.add.text(10, 10, 'Camera Position').setOrigin(0, 0));
+        }
     };
     /* update part */
     GameScene.prototype.update = function () {
         var fondo5a = this.object("fondo.5.a");
         // let fondo5b = this.object<Phaser.GameObjects.Image>("fondo.5.b");
-        var texto = this.object('texto.debug');
-        var camera = { x: this.cameras.main.scrollX, y: this.cameras.main.scrollY };
-        texto.setPosition(camera.x + 10, camera.y + 10);
-        texto.text = "Camera Position\t| x: " + Math.round(camera.x) + "\t| y: " + Math.floor(camera.y) + "\n";
-        texto.text += "Pasto 1 Position | x: " + Math.round(fondo5a.x) + " | y: " + Math.floor(fondo5a.y) + "\n";
-        // texto.text += `Fondo.5.b (Ground b) Position - x: ${Math.round(fondo5b.x)} - y: ${Math.floor(fondo5b.y)}\n`;
+        if (this.physics.config.debug) {
+            var texto = this.object('texto.debug');
+            var camera = { x: this.cameras.main.scrollX, y: this.cameras.main.scrollY };
+            texto.setPosition(camera.x + 10, camera.y + 10);
+            texto.text = "Camera Position\t| x: " + Math.round(camera.x) + "\t| y: " + Math.floor(camera.y) + "\n";
+            texto.text += "Pasto 1 Position | x: " + Math.round(fondo5a.x) + " | y: " + Math.floor(fondo5a.y) + "\n";
+            // texto.text += `Fondo.5.b (Ground b) Position - x: ${Math.round(fondo5b.x)} - y: ${Math.floor(fondo5b.y)}\n`;
+        }
         this.object("fondo.1").setPosition(camera.x * 1 - 100, camera.y - 100);
         this.object("fondo.2").setX(camera.x * 0 /* .04 */);
         this.object("fondo.3").setX(camera.x * 0 /* .08 */);
