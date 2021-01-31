e86aab8eea19fc2a7bc49e58be990e421da937fc
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-01-31 18:53:31 -0500

    add project

--- ts/cargar-fondo.ts
+++ ts/cargar-fondo.ts
@@ -9,8 +9,16 @@ function cargarFondo(game: GameScene) {
     )
   );
 
-  var fondo2 = game.object(
-    'fondo.2',
+  var fondo2a = game.object(
+    'fondo.2.a',
+    game.add.image(
+      0,
+      0,
+      'fondo.2'
+    )
+  );
+  var fondo2b = game.object(
+    'fondo.2.b',
     game.add.image(
       0,
       0,
@@ -18,8 +26,16 @@ function cargarFondo(game: GameScene) {
     )
   );
 
-  var fondo3 = game.object(
-    'fondo.3',
+  var fondo3a = game.object(
+    'fondo.3.a',
+    game.add.image(
+      0,
+      0,
+      'fondo.3'
+    )
+  );
+  var fondo3b = game.object(
+    'fondo.3.b',
     game.add.image(
       0,
       0,
@@ -27,8 +43,16 @@ function cargarFondo(game: GameScene) {
     )
   );
 
-  var fondo4 = game.object(
-    'fondo.4',
+  var fondo4a = game.object(
+    'fondo.4.a',
+    game.add.image(
+      0,
+      0,
+      'fondo.4'
+    )
+  );
+  var fondo4b = game.object(
+    'fondo.4.b',
     game.add.image(
       0,
       0,
@@ -73,23 +97,33 @@ function cargarFondo(game: GameScene) {
 
 
   fondo1.displayWidth = game.scale.width + 200;
-  fondo2.displayWidth = game.scale.width;
-  fondo3.displayWidth = game.scale.width;
-  fondo4.displayWidth = game.scale.width;
+  fondo2a.displayWidth = game.scale.width;
+  fondo2b.displayWidth = game.scale.width;
+  fondo3a.displayWidth = game.scale.width;
+  fondo3b.displayWidth = game.scale.width;
+  fondo4a.displayWidth = game.scale.width;
+  fondo4b.displayWidth = game.scale.width;
   fondo5a.displayWidth = game.scale.width;
+  fondo5b.displayWidth = game.scale.width;
   fondo5b.displayWidth = game.scale.width;
 
   fondo1.displayHeight = game.scale.height + 200;
-  fondo2.displayHeight = game.scale.height;
-  fondo3.displayHeight = game.scale.height;
-  fondo4.displayHeight = game.scale.height;
+  fondo2a.displayHeight = game.scale.height;
+  fondo2b.displayHeight = game.scale.height;
+  fondo3a.displayHeight = game.scale.height;
+  fondo3b.displayHeight = game.scale.height;
+  fondo4a.displayHeight = game.scale.height;
+  fondo4b.displayHeight = game.scale.height;
   fondo5a.displayHeight = game.scale.height;
   fondo5b.displayHeight = game.scale.height;
 
-  fondo2.setOrigin(0, 0);
   fondo1.setOrigin(0, 0);
-  fondo3.setOrigin(0, 0);
-  fondo4.setOrigin(0, 0);
+  fondo2a.setOrigin(0, 0);
+  fondo2b.setOrigin(0, 0);
+  fondo3a.setOrigin(0, 0);
+  fondo3b.setOrigin(0, 0);
+  fondo4a.setOrigin(0, 0);
+  fondo4b.setOrigin(0, 0);
   fondo5a.setOrigin(0, 0);
   fondo5b.setOrigin(0, 0);
 
