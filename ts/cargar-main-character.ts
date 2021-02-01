046f6eb92c797a3f7301ede57f40d280099eb781
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-02-01 16:09:41 -0500

    add project

---
+++ ts/cargar-main-character.ts
@@ -0,0 +1,38 @@
+function cargarMainCharacter(game: GameScene) {
+
+  /* CREATE */
+  var mainChar = game.object(
+    "character.main",
+    game.physics.add.sprite(game.scale.width / 2, game.scale.height / 2, 'character.main.idle')
+  );
+
+  /* OPTIONS */
+  mainChar.setOrigin(0, 1);
+
+  /* ANIMS */
+  mainChar.anims.create({
+    key: 'idle',
+    frames: game.anims.generateFrameNumbers('character.main.idle', { start: 0, end: 3 }),
+    frameRate: 10,
+    repeat: -1,
+  })
+
+  mainChar.anims.create({
+    key: 'attack',
+    frames: game.anims.generateFrameNumbers('character.main.attack.1', { start: 0, end: 5 }),
+    frameRate: 10
+  })
+
+  mainChar.setScale(2);
+
+  mainChar.addListener('animationcomplete', function () {
+    eval("console.log('anim complete', this)")
+    mainChar.anims.play('idle');
+  });
+
+  mainChar.anims.play('attack');
+
+  /* CAMERA FOLLOW */
+  game.cameras.main.startFollow(mainChar, false, 0.3, 0.3)
+
+}
