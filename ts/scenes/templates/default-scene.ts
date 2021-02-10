0ad6e532ef2e0ff305f4ce0b2660b6157e7dba4d
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-02-10 18:15:08 -0500

    add project

--- ts/scenes/templates/default-scene.ts
+++ ts/scenes/templates/default-scene.ts
@@ -45,70 +45,77 @@ export default class DefaultScene extends Phaser.Scene {
     }
   }
 
-  isDebugModeOn(() => void): void {
+  /**
+   * isDebugModeOn
+   */
+  public isDebugModeOn(callback: (text: Phaser.GameObjects.Text) => any): void {
+    if (this.dato<boolean>('debug mode')) {
 
-  };
+      callback(this.object<Phaser.GameObjects.Text>('texto.debug'))
 
-centerX: number;
-centerY: number;
+    }
+  }
 
-_object: {
-  object: { [c: string]: any },
-  data: { [c: string]: any }
-}
+  centerX: number;
+  centerY: number;
 
-/**
- * should override
- * @override
- */
-_preload: () => void
+  _object: {
+    object: { [c: string]: any },
+    data: { [c: string]: any }
+  }
+
+  /**
+   * should override
+   * @override
+   */
+  _preload: () => void
   /**
    * should override
    * @override
    */
   _create: () => void
-    /**
-     * should override
-     * @override
-     */
-    _update: () => void
+  /**
+   * should override
+   * @override
+   */
+  _update: () => void
 
-      /**
-       * preload
-       */
-      public preload() {
-  this.load.setCORS('anonymous');
-  this.load.setBaseURL('https://pagina-simx72-aba9b.web.app/mygame-assets/')
+  /**
+   * preload
+   */
+  public preload() {
+    this.load.setCORS('anonymous');
+    this.load.setBaseURL('https://pagina-simx72-aba9b.web.app/mygame-assets/')
 
-  this.load.on('loaderror', (file: any) => {
-    console.log(file)
-  })
+    this.load.on('loaderror', (file: any) => {
+      console.log(file)
+    })
 
-  this.load.xhr.withCredentials = false;
+    this.load.xhr.withCredentials = false;
 
-  this.centerX = this.game.scale.width / 2;
-  this.centerY = this.game.scale.height / 2;
-  this._preload()
+    this.centerX = this.game.scale.width / 2;
+    this.centerY = this.game.scale.height / 2;
+    this._preload()
 
-}
+  }
   /**
    * create
    */
   public create() {
-  this.dato('debug mode', this.physics.config.debug)
-  if (this.dato<boolean>('debug mode')) {
-    this.object(
-      'texto.debug',
-      this.add.text(10, 10, 'Camera Position').setOrigin(0, 0)
-    );
+    this.dato('debug mode', this.physics.config.debug)
+    if (this.dato<boolean>('debug mode')) {
+      this.object(
+        'texto.debug',
+        this.add.text(10, 10, 'Camera Position').setOrigin(0, 0)
+      );
+    }
+    this._create()
   }
-  this._create()
-}
   /**
    * update
    */
   public update() {
-  this._update()
-}
+    this._update()
+  }
 
 }
