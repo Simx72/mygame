fccfbbbf36ec04b537a53888d28d3efa210376fb
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-04 20:17:45 -0500

    add to project

--- ts/functions/update-fondo.ts
+++ ts/functions/update-fondo.ts
@@ -1,45 +1,45 @@
 import LevelScene from '../scenes/templates/level-scene';
-export default function updateFondo(game: LevelScene) {
+export default function updateFondo(this: LevelScene) {
 
-  let camera = { x: game.cameras.main.scrollX, y: game.cameras.main.scrollY };
+  let camera = { x: this.cameras.main.scrollX, y: this.cameras.main.scrollY };
 
-  game.object<Phaser.GameObjects.Image>("fondo.1").setPosition(camera.x * 1 - 100, camera.y - 100);
-  let x2 = game.object<Phaser.GameObjects.Image>("fondo.2.a").setX(
-    game.calcularPos(
+  this.object<Phaser.GameObjects.Image>("fondo.1").setPosition(camera.x * 1 - 100, camera.y - 100);
+  let x2 = this.object<Phaser.GameObjects.Image>("fondo.2.a").setX(
+    this.calcularPos(
       camera.x, 0.04,
-      game.scale.width
+      this.scale.width
     )
   ).x;
-  game.object<Phaser.GameObjects.Image>("fondo.2.b").setX(
-    x2 + game.scale.width
+  this.object<Phaser.GameObjects.Image>("fondo.2.b").setX(
+    x2 + this.scale.width
   );
-  let x3 = game.object<Phaser.GameObjects.Image>("fondo.3.a").setX(
-    game.calcularPos(
+  let x3 = this.object<Phaser.GameObjects.Image>("fondo.3.a").setX(
+    this.calcularPos(
       camera.x, 0.08,
-      game.scale.width
+      this.scale.width
     )
   ).x;
-  game.object<Phaser.GameObjects.Image>("fondo.3.b").setX(
-    x3 + game.scale.width
+  this.object<Phaser.GameObjects.Image>("fondo.3.b").setX(
+    x3 + this.scale.width
   );
-  let x4 = game.object<Phaser.GameObjects.Image>("fondo.4.a").setX(
-    game.calcularPos(
-      camera.x, 0.16, game.scale.width
+  let x4 = this.object<Phaser.GameObjects.Image>("fondo.4.a").setX(
+    this.calcularPos(
+      camera.x, 0.16, this.scale.width
     )
   ).x;
-  game.object<Phaser.GameObjects.Image>("fondo.4.b").setX(
-    x4 + game.scale.width
+  this.object<Phaser.GameObjects.Image>("fondo.4.b").setX(
+    x4 + this.scale.width
   );
-  let x5 = game.object<Phaser.GameObjects.Image>("fondo.5.a").setX(
-    game.calcularPos(
-      camera.x, 0.32, game.scale.width
+  let x5 = this.object<Phaser.GameObjects.Image>("fondo.5.a").setX(
+    this.calcularPos(
+      camera.x, 0.32, this.scale.width
     )
   ).x;
-  game.object<Phaser.GameObjects.Image>("fondo.5.b").setX(
-    x5 + game.scale.width
+  this.object<Phaser.GameObjects.Image>("fondo.5.b").setX(
+    x5 + this.scale.width
   );
 
-  if (game.physics.config.debug) {
+  if (this.physics.config.debug) {
     // let texto = this.object<Phaser.GameObjects.Text>('texto.debug');
   }
 
