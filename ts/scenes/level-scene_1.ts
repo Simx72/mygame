13376ec416a8f9966a592c2fe3ffc9fc29b3575f
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-04 20:05:08 -0500

    add to project

--- ts/scenes/level-scene_1.ts
+++ ts/scenes/level-scene_1.ts
@@ -40,24 +40,6 @@ export default class LevelScene_1 extends LevelScene {
       texto.text += `[game]: Camera Position - x ${Math.floor(camera.x)} | y ${Math.floor(camera.y)} \n`;
     });
 
-    var mainChar = this.object<Phaser.Types.Physics.Arcade.ImageWithDynamicBody>("character.main");
-
-    this.isDebugModeOn(texto => {
-      texto.text += `[game]: Player - x ${Math.floor(mainChar.x)} | y ${Math.floor(mainChar.y)} \n`;
-    });
-
-    const cursorKeys = this.input.keyboard.createCursorKeys();
-
-    if (cursorKeys.right.isDown && cursorKeys.left.isUp) {
-      mainChar.body.setVelocityX(500)
-      mainChar.setFlipX(false)
-    } else if (cursorKeys.left.isDown && cursorKeys.right.isUp) {
-      mainChar.body.setVelocityX(-500)
-      mainChar.setFlipX(true)
-    } else {
-      mainChar.body.setVelocityX(0)
-    }
-
   }
 
 }
